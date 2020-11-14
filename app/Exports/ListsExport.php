c32882a2eff7f714f21d72d0c17ac9e3389dfc93
faissaloux <fwahabali@gmail.com>
2020-11-14 14:57:07 +0100

    update: export Excel with headings

--- app/Exports/ListsExport.php
+++ app/Exports/ListsExport.php
@@ -6,9 +6,43 @@ use App\Lists;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Http\Request as HttpRequest;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ListsExport implements FromCollection
+class ListsExport implements FromCollection, WithHeadings
 {
+
+    public function headings(): array
+    {
+        return [
+            'name',
+            'adress',
+            'note',
+            'phone',
+            'source',
+            'provider_id',
+            'employee_id',
+            'handler',
+            'city_id',
+            'laivraison',
+            'cancel_reason',
+            'history',
+            'product',
+            'city',
+            'quantity',
+            'price',
+            'status',
+            'unanswered_at',
+            'accepted_at',
+            'verified_at',
+            'delivered_at',
+            'deleted_at',
+            'canceled_at',
+            'duplicated_at',
+            'checked_at',
+            'recall_at',
+        ];
+    }
+    
     /**
     * @return \Illuminate\Support\Collection
     */
