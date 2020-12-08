4be2685511359b24db15f62648d5991955edf70c
faissaloux <fwahabali@gmail.com>
2020-12-08 11:35:54 +0100

    fix: show provider name in cities table

--- app/Cities.php
+++ app/Cities.php
@@ -7,8 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Cities extends Model
 {
 
-	protected $guarded = ['id'];
+    protected $guarded = ['id'];
+    
     public function user(){
         return $this->belongsTo('\App\User', 'provider_id')->withDefault(['name' => 'N-A']);
+    }
+
+    public function provider(){
+        return $this->belongsTo('\App\Provider', 'provider_id')->withDefault(['name' => 'N-A']);
     }
 }
